486a1ad3f7ad7ed2e05a988d764403ba5abf194d
ljbha007 <ljbha007@localhost>
2011-10-01 05:06:01 +0800

    ???????????????????????BUG

--- createmenu.js
+++ createmenu.js
@@ -1,34 +1,36 @@
 ﻿function onClick(info, tab){
 	// When user clicks on the menu item
-	var existingNotifications =  chrome.extension.getViews({type:"notification"});
-	for(var i = 0; i< existingNotifications.length ; i ++){
-		var existing = existingNotifications[i];
-		existing.close();
-	};
-	var notification = webkitNotifications.createHTMLNotification("popup.html");
-	notification.addEventListener("display", function(){
-		console.log("notification displayed!");
-		var win = null;
-		waitAndQuery();
-		function waitAndQuery(){
-			if(win = chrome.extension.getViews({type:"notification"})[0]){
-				console.log(info.selectionText);
-				win.query(info.selectionText);
-				var toHide = ["logo", "word", "search"];
-				for(var i = 0; i< toHide.length ; i ++){
-					var id = toHide[i];
-					var elem = win.document.getElementById(id);
-					//console.log(id);
-					elem.setAttribute("style", "display:none");
-				}
-			}else{
-				setTimeout(waitAndQuery, 200);
-			}
+	// close all existing notifications
+	function queryMyWindow(win){
+		win.query(info.selectionText);
+		var toHide = ["logo", "word", "search"];
+		for(var i = 0; i< toHide.length ; i ++){
+			var id = toHide[i];
+			var elem = win.document.getElementById(id);
+			elem.setAttribute("style", "display:none");
 		}
-	});
-	notification.show();
-	console.log(notification);
-	//console.log("Item was clicked");
+	}
+	var existingNotifications =  chrome.extension.getViews({type:"notification"});
+	if(existingNotifications.length > 0){
+		var win = existingNotifications[0];
+		queryMyWindow(win);
+	}else{
+		// create a new one
+		var notification = webkitNotifications.createHTMLNotification("popup.html");
+		notification.addEventListener("display", function(){
+			var win = null;
+			waitAndQuery();
+			function waitAndQuery(){
+				if(win = chrome.extension.getViews({type:"notification"})[0]){
+					console.log(info.selectionText);
+					queryMyWindow(win);
+				}else{
+					setTimeout(waitAndQuery, 200);
+				}
+			}
+		});
+		notification.show();
+	}
 }
 var id = chrome.contextMenus.create({
 	"title" : "使用扇贝查词",
